8e01eb19af03991816049ac5b2f4804e9a215b34
deriggsm <128313185+deriggsm@users.noreply.github.com>
2025-04-24 00:19:06 -0400

    Add files via upload

---
+++ MJD2.py
@@ -0,0 +1,10 @@
+## MJD 2
+## Comments
+firstgrade = "one hundred and two"
+secondgrade = "one hundred"
+firstgrade = "one hundred"
+# Show off
+
+if firstgrade == secondgrade:
+	print(firstgrade)
+
